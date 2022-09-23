4573b033f85c44a3abcf14d11023e85927c0b50c
hemanthadepu08 <76736013+hemanthadepu08@users.noreply.github.com>
2022-09-23 21:09:42 +0530

    Save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import "./styles.css";
-import React from "react"
+import React from "react";
 
 //With JSX
-function App(){
-return(<React.Fragment>
-  <h1>Reactjs</h1>
-  <h1>JAVA</h1>
-  </React.Fragment>
-)
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Reactjs</h1>
+      <h1>JAVA</h1>
+    </React.Fragment>
+  );
 }
-export default App
+export default App;
 
 // //without JSX CODE
 // export default function App() {
 //   return( React.createElement(React.Fragment, null, React.createElement("h1", {
 //     id: "name"
-//   }, "hemath"), 
+//   }, "hemath"),
 //   React.createElement("h2", null, "Reactjs"),
 //   React.createElement("h2", null, "Java")
 //   )
-  
+
 //   )
-   
+
 // }
---
+++ src/Myfile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Myfile() {
+  return <h1>Reactjs</h1>;
+}
+
+export default Myfile;
