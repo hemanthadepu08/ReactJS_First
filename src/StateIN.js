ce1f13b1a81e9855970d0983ce6a5482dd25b823
hemanthadepu08 <76736013+hemanthadepu08@users.noreply.github.com>
2022-09-24 21:44:07 +0530

    new

--- src/StateIN.js
+++ src/StateIN.js
@@ -1,18 +1,24 @@
 import React from "react";
 
 class StateIN extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    //state initialised
     this.state = {
       name: "Hemanth Adepu",
       Company: "Prodapt"
     };
   }
+  //update state
+  updateState = () => {
+    this.setState({ name: "New name" });
+  };
   render() {
     return (
       <>
         <h1>{this.state.name}</h1>
         <h1>{this.state.Company}</h1>
+        <button onClick={this.updateState}>Click me!</button>
       </>
     );
   }
